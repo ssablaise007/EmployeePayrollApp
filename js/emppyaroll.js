a4475d72ccb0f6de0cb2efe3ccd2ebc3173dcf57
ssablaise007 <ssablaise007@gmail.com>
2021-09-02 19:04:13 +0530

    Added : UC3 Creating EmployeePayroll Object

--- js/emppyaroll.js
+++ js/emppyaroll.js
@@ -46,7 +46,7 @@ class EmployeePayrollData{
     ", salary =" + this.salary +" , startDate=" + empDate +" , note=" +this.note;
  }
  // UC2 //
- window.addEventListener('DOMcontentLOaded', (event) ==> {
+ window.addEventListener('DOMContentLOaded' (event) => {
      const name =document.querySelector('#name');
      const textError = document.querySelector('.text-error');
      name.addEventListener('input', function(){
@@ -67,3 +67,49 @@ class EmployeePayrollData{
       output.textContent = salary.value;
   });
  });
+
+ const save = () =>{
+     try{
+         let EmployeePayrollData = createEmployeePayroll();
+     }catch(e) {
+         return;
+     }
+ }
+ const createEmployeePayroll = () => {
+     let employeePayrollData= new EmployeePayrollData();
+     try{
+         employeePayrollData.name =  getInputValueById('#name');
+     }catch (e){
+         setTextValue('.text-error',e);
+         throw e;
+     }
+     employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+     employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+     employeePayrollData.department = getSelectedValues('[name=department]');
+     employeePayrollData.salary = getInputValueById('#salary');
+     employeePayrollData.note = getInputValueById('#notes');
+     let date = getInputValueById('#day')+" "+getInputValueById('#month')+" "+getInputValueById('#year');
+     employeePayrollData.date = Date.parse(date);
+     alert(employeePayrollData.toString());
+     return employeePayrollData;
+ }
+
+ const getSelectedValues = (propertyValue) => {
+     let allItems = document.querySelectorAll(propertyValue);
+     let selItems = [];
+     allItems.forEach(item => {
+         if(item.checked) selItems.push(item.value);
+     });
+     return selItems;
+ }
+ /// UC3  ///
+/**
+ * 1.querySelector is the newer feature 
+ * 2.the querym method can be used when selecting by element
+ * nesting , or class name.
+ * 3.query selector lets you find elements with rules tha can't be exprressed with getElementById
+ */
+const getInputValueById = (id) => {
+    let value =  document.querySelector(id).value;
+    return value;
+}
