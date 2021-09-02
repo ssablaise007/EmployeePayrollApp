dd3e12df571c3d0766c68398ff9bcfe95031b56a
ssablaise007 <ssablaise007@gmail.com>
2021-09-02 17:56:57 +0530

    Added : UC1 with New Attributes

---
+++ js/emppyaroll.js
@@ -0,0 +1,49 @@
+class EmployeePayrollData{
+ 
+    // getter and setter method
+    getid(){ return this._id; }
+    set _id(id){
+        this._id=id;
+    }
+    get name(){ returnthis._name;}
+    set name(name){
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$')
+        if (nameRegex.test(name))
+        this._name = name;
+        else throw 'Name is Incorrect !';
+    }
+    get profilePic(){ return this._profilePic; }
+    set profilePic(profilePic){ 
+        this._profilePic = profilePic;
+     }
+     set gender () { return this._gender; }
+     set gender(gender){
+         this._gender = gender;
+     }
+     set department () { return this._department; }
+     set department(department){
+         this._department = department;
+     }
+     set salary () { return this._salary; }
+     set salary(salary){
+         this._salary = salary;
+     }
+     set note () { return this._note; }
+     set note(note){
+         this._note = note;
+     }
+     set startDate () { return this._startDate; }
+     set startDate(startDate){
+         this._startDate = startDate;
+     }
+     // Method 
+ toString(){
+     const options ={ year: 'numeric' , month:'long' , day:'numeric'};
+     const empDate = !this.startDate ? "undefined" :
+                        this.startDate.toLocalDateString("en-US", options);
+    return "id=" +this.id+",name=" + this.name +", gender=" +this.gender +
+    ", profilePic= '" +this.profilePic +",department=" + this.department +
+    ", salary =" + this.salary +" , startDate=" + empDate +" , note=" +this.note;
+ }        
+}
+
